9577688d815569262ffc57cfd4cb044d0b5e5699
Mukul <74971960+mk-mukul@users.noreply.github.com>
2022-09-12 03:19:41 +0530

    add command upd to upload file

--- client.py
+++ client.py
@@ -1,73 +1,105 @@
 import socket
 import json
 import time
+import os
 
 HEADER = 64
 PORT = 5050
-FORMAT = 'utf-8' # encode and decode format
-DISCONNECT_MSG = "!DISCONNECT"
 SERVER = 'acer.mkmukul.com'
 ADDR = (SERVER, PORT)
+FORMAT = 'utf-8' # encode and decode format
 
 client =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(ADDR)
 print(f"[CONNECTED] {ADDR} connected.")
 
+
 def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
+    send_msg = json.dumps(msg)
+    msg_length = len(send_msg)
     send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length)) #make the lenght of string of lenth 64 byte
+    send_length += b' ' * (HEADER - len(send_length)) # to make string 64 byte
     client.send(send_length)
-    client.send(message)
+    client.send(send_msg.encode(FORMAT))
+
+
+def receive():
+    data_length = client.recv(HEADER).decode(FORMAT)
+    if data_length:
+        data_length = int(data_length)
+        data = client.recv(data_length).decode(FORMAT)
+        data = json.loads(data)
+        return data
+
+
+def download_file():
+    print(f"File downloading from {SERVER}")
+    print(f"Downloading...")
+    file_name = receive()
+    file_size = receive()
+    time.sleep(1)
+    with open("./"+file_name, mode='w', encoding='utf-8') as file:
+        start_time = time.time()
+        data = receive()
+        file.write(data)
+        end_time = time.time()
+        total_time = end_time - start_time
+    print(f"File received from {SERVER} in {total_time} seconds")
+    return 1
+
+
+def upload_file(file_name): 
+    print(f"File uploading...")     
+    send(file_name)
+    file_size = os.path.getsize(file_name)
+    send(file_size)
+    with open(file_name, mode='r', encoding='utf-8') as file:
+        start_time = time.time()
+        data = file.read()
+        send(data)
+        end_time = time.time()
+        total_time = end_time - start_time
+    print(f"File uploaded to {SERVER} in {total_time} seconds")
+    return 1
+    
+
+connected = True
+while connected:
+    msg = str(input(f"{SERVER}:~>> "))
+
+    if msg == "exit":
+        send(msg)
+        print(f"[DISCONNECTED] {ADDR} disconnected.")
+        print(receive())
+        client.close()
+        connected = False
+        break
+
+    if msg.split()[0] == "upd":
+        if msg.split()[1] in os.listdir():
+            if os.path.isfile(msg.split()[1]):
+                send(msg)
+                upload_file(msg.split()[1])
+                print("Status: ", receive())
+                continue
+            else:
+                print("File not present")
+                continue
+        else:
+            print("File not present")
+            continue
+
+    send(msg)
     data = receive()
     if type(data)==list:
         for i in data:
             print(f"{i}", end="\t")
         print()
-        return
-    print(data)
-    if data == "File sending...":
+    elif data == "File sending...":
         download_file()
-
-def receive():
-    msg_length = client.recv(HEADER).decode(FORMAT)
-    if msg_length:
-        msg_length = int(msg_length)
-        msg = client.recv(msg_length).decode(FORMAT)
-        msg = json.loads(msg)
-        return msg
-
-def download_file():
-    file_name = receive()
-    file_size = receive()
-    print(file_name)
-    print(file_size)
-    time.sleep(1)
-    with open("./rec/"+file_name, mode='w', encoding='utf-8') as file:
-        c = 0
-        start_time = time.time()
-        # while c<1:
-        data = receive()
-        # if not data:
-        #     break
-        file.write(data)
-        # c+=len(data)
-        end_time = time.time()
-        total_time = end_time - start_time
-    print(f"File received in ", total_time)
-    
-
-
-connected = True
-while connected:
-    msg = str(input(f"{SERVER}:~>> "))
-    if msg == "exit":
-        send(DISCONNECT_MSG)
-        print(f"[DISCONNECTED] {ADDR} disconnected.")
-        client.close()
-        connected = False
+        print("Status: ", receive())
     else:
-        send(msg)
+        print(data)
+        
 
--- server.py
+++ server.py
@@ -4,22 +4,18 @@ import os
 import json
 import time
 
-HEADER = 64 # tells client that first 64 bytes is the number that tells number of bytes thats going to received 
-# if msg length is more then 64 bytes the there is a problem to represent or tell the server
-
+HEADER = 64 
 PORT = 5050
 SERVER = 'acer.mkmukul.com'
-# SERVER = socket.gethostbyname(socket.gethostname())
-# print(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8' # encode and decode format
-DISCONNECT_MSG = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-def tart():
-    server.bind(ADDR)
-tart()
 
+
+def bind_server(ADDR):
+    server.bind(ADDR)
+bind_server(ADDR)
 
 
 def start():
@@ -36,25 +32,44 @@ def start():
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MSG:
-                connected = False
-                send(conn, addr, "Connection closed form the server")
-                break
-            print(f"[{addr}] {msg}")
-            return_msg = handle_command(conn, addr, msg)
-            send(conn, addr, return_msg)
+    while connected:    
+        data = receive(conn, addr)
+        if data == "exit":
+            connected = False
+            send(conn, addr, "Connection closed form the server")
+            break
+        print(f"[{addr}] {data}")
+        return_data = handle_command(conn, addr, data)
+        send(conn, addr, return_data)
     conn.close()
+    print(f"[DISCONNECTED] {addr} disconnected")
     # print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 2}")
+
+
+def send(conn, addr, msg):
+    send_msg = json.dumps(msg)
+    msg_length = len(send_msg)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    conn.send(send_length)
+    conn.send(send_msg.encode(FORMAT))
+
+
+def receive(conn, addr):
+    data_length = conn.recv(HEADER).decode(FORMAT)
+    if data_length:
+        data_length = int(data_length)
+        data = conn.recv(data_length).decode(FORMAT)
+        data = json.loads(data)
+        return data
+
 
 def handle_command(conn, addr, cmd):
     command = cmd.split()
+
     if not command:
         return ""
+
     if command[0] == "cwd":
         return os.getcwd()
 
@@ -63,68 +78,65 @@ def handle_command(conn, addr, cmd):
 
     if command[0] == "cd":
         if len(command) < 2:
-            return "Invalid argument"
+            return "Status: NOK"
         if command[1] in os.listdir():
             os.chdir(command[1])
-            return os.getcwd()
+            return "Status: OK"
         elif command[1] == "../":
             os.chdir(command[1])
-            return os.getcwd()
+            return "Status: OK"
         else:
-            return "Directory not present"
+            return "Directory not present\nStatus: NOK"
 
     if command[0] == "dwd":
         if len(command) < 2:
             return "Invalid argument"
-        return send_file(conn, addr, command[1])
+        if command[1] in os.listdir():
+            if os.path.isfile(command[1]):
+                return send_file(conn, addr, command[1])
+        return "File not present"
 
     if command[0] == "upd":
         if len(command) < 2:
             return "Invalid argument"
-        return load_file(conn, addr, command[1])
+        return receive_file(conn, addr)
 
     return "Invalid command"
 
 
-def send(conn, addr, msg):
-    send_msg = json.dumps(msg)
-    msg_length = len(send_msg)
-    send_length = str(msg_length).encode(FORMAT)
-    # print(f"Send length: {send_length} || lenght of send lenght: {len(send_length)}")
-    send_length += b' ' * (HEADER - len(send_length))
-    # print(len(send_length))
-
-    conn.send(send_length)
-    conn.send(send_msg.encode(FORMAT))
-
 def send_file(conn, addr, file_name):
-    # print(file_name)
-    if file_name in os.listdir():
-        if os.path.isfile(file_name):
-            print("Sending file...")
-            send(conn, addr, "File sending...")
-            send(conn, addr, file_name)
-            file_size = os.path.getsize(file_name)
-            print(file_size)
-            send(conn, addr, file_size)
-            with open(file_name, mode='r', encoding='utf-8') as file:
-                c = 0
-                start_time = time.time()
-                # while c <= file_size:
-                data = file.read()
-                # if not data:
-                #     break
-                send(conn, addr, data)
-                    # c+=len(data)
-                end_time = time.time()
-                total_time = end_time - start_time
-            print(f"File sent to {addr} in {total_time}")
-            return "File send in " + str(total_time)
-    return "File not present"
+    print(f"Sending file to {addr}")
+    print(f"Sending...")
+    send(conn, addr, "File sending...")
+    send(conn, addr, file_name)
+    file_size = os.path.getsize(file_name)
+    send(conn, addr, file_size)
+    with open(file_name, mode='r', encoding='utf-8') as file:
+        start_time = time.time()
+        data = file.read()
+        send(conn, addr, data)
+        end_time = time.time()
+        total_time = end_time - start_time
+    print(f"File send to {addr} in {total_time} seconds")
+    print("Status: OK")
+    return "OK"
 
-def load_file(conn, addr, file_name):
-    print(file_name)
-    return "file not received"
+
+def receive_file(conn, addr):
+    print(f"Receiving file form {addr}")
+    print(f"Receving...")
+    file_name = receive(conn, addr)
+    file_size = receive(conn, addr)
+    with open("./"+file_name, mode='w', encoding='utf-8') as file:
+        start_time = time.time()
+        data = receive(conn, addr)
+        file.write(data)
+        end_time = time.time()
+        total_time = end_time - start_time
+    print(f"File received form {addr} in {total_time} seconds")
+    print("Status: OK")
+    return "OK"
+
 
 print("[STARTING] server is starting...")
 start()
